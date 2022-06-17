c5e83151bbc308c6794bac30f0123af07b55297c
HarshVardhan Jain <jainharsh3716@gmail.com>
2022-06-17 09:38:22 +0530

    removed some used files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
@@ -13,8 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
